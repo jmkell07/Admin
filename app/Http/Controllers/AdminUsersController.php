f7a91aead9b092fa851a433688ee302ae5bb7ee1
John Kelly <jmkell07@smumn.edu>
2018-01-28 16:03:59 -0600

    User Photo

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -8,6 +8,7 @@ use App\Http\Requests;
 use App\Http\Requests\UserRequest;
 use App\User;
 use App\Role;
+use App\Photo;
 
 class AdminUsersController extends Controller
 {
@@ -21,9 +22,17 @@ class AdminUsersController extends Controller
         $roles = Role::lists('name', 'id')->all();
         return view('admin.users.create', compact('roles'));
     }
-    public function store(UserRequest $request)
+    public function store(UserRequest $req)
     {
-        User::create($request->all());
+        $input = $req->all();
+        if($file = $req->file('profile_pic')){
+            $name = time() . $file->getClientOriginalName();
+            $file->move('images', $name);
+            $photo = Photo::create(['path'=>$name]);
+            $input['photo_id'] = $photo->id;
+        }
+        $input['password'] = bcrypt($req->password);
+        User::create($input);
         return redirect('/admin/users');
     }
     public function show($id)
